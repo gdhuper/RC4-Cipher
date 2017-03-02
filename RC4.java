ecdf528761d84de8e7e7199a1455b6ea77e7f0d5
Gurpreet Singh <gdhuper@gmail.com>
2017-03-01 19:34:15 -0800

    final working code

--- RC4.java
+++ RC4.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RC4 {
@@ -15,7 +16,7 @@ public class RC4 {
 	}
 
 	public void initialize(byte[] key) {
-		
+
 		int val = 0;
 		for (int i = 0; i < 16; i++) {
 			for (int j = 0; j < 16; j++) {
@@ -26,14 +27,14 @@ public class RC4 {
 		}
 
 		// Debugging
-		//System.out.println("Initial Lookup table");
-		//printMatrix(S);
-		//System.out.println("Initial key table");
-		//printMatrix(K);
-		
-		//Initial permutation
-		int J = 0; //index to switch Ith index element with
-		int I = 0; 
+		System.out.println("Initial Lookup table");
+		printMatrix(S);
+		System.out.println("Initial key table");
+		printMatrix(K);
+
+		// Initial permutation
+		int J = 0; // index to switch Ith index element with
+		int I = 0;
 		for (I = 0; I < 16; I++) {
 			for (int j = 0; j < 16; j++) {
 				J = (J + S[I][j] + K[I][j]) % 256;
@@ -44,9 +45,35 @@ public class RC4 {
 		System.out.println("After initialization: \n");
 		printMatrix(S);
 		System.out.println("j after initialization: " + J);
-		System.out.println("i after initialization: " + (I*I-1) + "\n");
-		
-		
+		System.out.println("i after initialization: " + (I * I - 1) + "\n");
+
+	}
+	
+	
+	/**
+	 * generate byte streams
+	 * @param S lookup table
+	 * @param N number of bytes to generate
+	 */
+	public void generateByteStream(int[][] S, double N) {
+		System.out.println("Generating "+ N +" bytestreams");
+		int tempSize = (int) Math.sqrt(N);
+		ArrayList<Integer> K = new ArrayList<Integer>();
+		int i = 0;
+		int j = 0;
+		while(N > 0)
+		{
+			i = (i+1) % 256;
+			j = (j + S[i/16][i%16]) % 256;
+			swap(S, i/16, i%16, j);
+			int kthIdx = (S[i/16][i%16] + S[j/16][j%16]) % 256;
+			int k = S[kthIdx/16][kthIdx%16];
+			K.add(k);
+			N--;
+		}
+		System.out.println("i after generating " + tempSize + " bytestreams: " + i);
+		System.out.println("i after generating " + tempSize + " bytestreams: " + j);
+		printMatrix(S);
 
 	}
 
@@ -62,22 +89,50 @@ public class RC4 {
 	public void printMatrix(int[][] S) {
 		for (int i = 0; i < 16; i++) {
 			for (int j = 0; j < 16; j++) {
-				System.out.printf("%5s", S[i][j]);
-
+				System.out.printf("%5s", Integer.toHexString(S[i][j]));
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
 
-	public void generateByteStream(int i, int j, byte[] S) {
+	
 
+	public String getpText() {
+		return pText;
 	}
 
-	public void swap(int i, int j, byte[] S) {
-
+	public void setpText(String pText) {
+		this.pText = pText;
 	}
 
+	public byte[] getKeys() {
+		return keys;
+	}
+
+	public void setKeys(byte[] keys) {
+		this.keys = keys;
+	}
+
+	public int[][] getS() {
+		return S;
+	}
+
+	public void setS(int[][] s) {
+		S = s;
+	}
+
+	public int[][] getK() {
+		return K;
+	}
+
+	public void setK(int[][] k) {
+		K = k;
+	}
+
+	
+	
+	//Tester
 	public static void main(String[] args) {
 		boolean done = false;
 		String p = null;
@@ -85,7 +140,6 @@ public class RC4 {
 		byte[] keys = new byte[] { 0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F, 0x77 };
 
 		Scanner in = new Scanner(System.in);
-
 		System.out.println("Enter plaintext to encrypt (without spaces):");
 		while (!done) {
 			p = in.next();
@@ -109,7 +163,7 @@ public class RC4 {
 						i++;
 					}
 					done = true;
-					in.close();
+					//in.close();
 				} else {
 					System.out.println("Please select a valid option! (1 or 2)");
 					System.out.println(
@@ -122,6 +176,11 @@ public class RC4 {
 
 		RC4 rc = new RC4(p, keys);
 		rc.initialize(keys);
+		System.out.println("Enter number of bytestreams to generate (int):"); 
+		double numBytes = in.nextDouble();
+		
+		//generating bytestreams
+		rc.generateByteStream(rc.getS(), numBytes);
 
 		// For debugging
 		System.out.println("PlainText: \n" + p + "\n");
